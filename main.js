e7022d65c389f7ed31d700c2d12439521cbe76c6
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-06-24 23:19:55 -0400

    2

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 function challengeBanner(num){
 console.log('######### Challenge ' + num + " #########")
 }
-challengeBanner(1)
-
+const vowels = 'aeiou';
 
 function onlyVowels(str){
     // const str = "Regular expressions are for term 2."
-    const vowels = 'aeiou';
+
             let i = 0;
                 while(i < str.length){
                     if(vowels.includes(str[i])){
                         console.log(str[i])
                     }   
                             i++
-   
+
 }
 }
+challengeBanner(1)
 onlyVowels("Regular expressions are for term 2.")
+
+function firstFive(str){
+    let i = 0;
+        while(i < str.length){
+            if(vowels.includes(str[i]) && i < 5){
+                    console.log(str[i])
+            }
+                    i++;
+
+
+  }
+}
+challengeBanner(2)
+firstFive("Regular expressions are for term 2.")
